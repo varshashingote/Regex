a8f7e3233d4180915ab8b71a0b78ac3e274fc507
varsha Gunjal <varshashingote01@gmail.com>
2023-02-14 23:49:46 +0530

    [varsha] validation of first name

---
+++ RegularExpresion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpresion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            UserValidation userValidation = new UserValidation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstname = Console.ReadLine();
+            userValidation.ValidationFirstName(firstname);
+            Console.ReadLine();
+        }
+
+    
+    }
+}
---
+++ RegularExpresion/UserValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpresion
+{
+    public class UserValidation
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{3,}$";
+        public void ValidationFirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+
+        }
+    }
+}
